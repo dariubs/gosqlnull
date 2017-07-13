9b0f59aadde9a7d1474458e53faffd50139484fd
Dariush Abbasi <dariushem@yahoo.com>
2017-07-14 00:41:55 +0430

    add Bool & Float64

--- null.go
+++ null.go
@@ -6,17 +6,39 @@ import(
   "encoding/json"
 )
 
+type Bool struct {
+  sql.NullBool
+}
+
 type String struct {
   sql.NullString
+}
+
+type Float64 struct {
+  sql.NullFloat64
 }
 
 type Int64 struct {
   sql.NullInt64
 }
 
+func (out Bool) MarshalJSON() ([]byte, error) {
+  if out.Valid {
+    return json.Marshal(out.Bool)
+  }
+  return json.Marshal(nil)
+}
+
 func (out String) MarshalJSON() ([]byte, error) {
   if out.Valid {
     return json.Marshal(out.String)
+  }
+  return json.Marshal(nil)
+}
+
+func (out Float64) MarshalJSON() ([]byte, error) {
+  if out.Valid {
+    return json.Marshal(out.Float64)
   }
   return json.Marshal(nil)
 }
